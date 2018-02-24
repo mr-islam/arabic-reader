8f7ccb27fece1dadac18cb3a0ca85c976c2b996f
Naved Islam <navedcoded@gmail.com>
2018-02-24 22:48:55 +0500

    separating script, adding hide elemnt

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function showMessage(){
+  var message = document.getElementById("message").value;
+  display_message.innerHTML= message;
+  }
+
+function myFunction() {
+    var x = document.getElementById("user-input");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+} 
