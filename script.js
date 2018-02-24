090b47db77d9671562c07e3978157ca6b4bdfaa0
Naved Islam <navedcoded@gmail.com>
2018-02-24 22:53:31 +0500

    renamed hide function

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 
-function showMessage(){
+function showMessage() {
   var message = document.getElementById("message").value;
   display_message.innerHTML= message;
-  }
+}
 
-function myFunction() {
-    var x = document.getElementById("user-input");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
+function hideInput() {
+  var x = document.getElementById("user-input");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 } 
