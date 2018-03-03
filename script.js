3a7f1f36f995a20e78b1c6a71299e77fd667b9df
Naved Islam <navedcoded@gmail.com>
2018-03-03 22:07:50 +0500

    update hide script

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
-
 function showMessage() {
   var message = document.getElementById("message").value;
   document.getElementById("display-message").innerHTML = message;
 }
-
 function hideInput() {
-  var inputElems = document.getElementsByClassName("input");
-  if (inputElems.style.display === "none") {
-    inputElems.style.display = "block";
-  } else {
-    inputElems.style.display = "none";
-  }
+  var button = document.getElementById('button');
+  button.style.display='none';
+  var message = document.getElementById('message');
+  message.style.display='none';
 } 
