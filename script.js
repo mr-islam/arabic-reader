de9b16634df216dea04c812c5399ba6cdd79e8a5
Naved Islam <navedcoded@gmail.com>
2018-03-09 23:04:49 +0500

    autoheight of textarea

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 function showMessage() {
-  var message = document.getElementById("message").value;
-  document.getElementById("display-message").innerHTML = message;
+    var message = document.getElementById("message").value;
+    document.getElementById("display-message").innerHTML = message;
 }
 function hideInput() {
-  var button = document.getElementById('button');
-  button.style.display='none';
-  var message = document.getElementById('message');
-  message.style.display='none';
-} 
+    var button = document.getElementById('button');
+    button.style.display='none';
+    var message = document.getElementById('message');
+    message.style.display='none';
+}
+
+//for no scroll-bar, auto expanding height of textarea
+var tx = document.getElementsByTagName('textarea');
+for (var i = 0; i < tx.length; i++) {
+    tx[i].setAttribute('style', 'height:' + (tx[i].scrollHeight) + 'px;overflow-y:hidden;');
+    tx[i].addEventListener("input", OnInput, false);
+}
+function OnInput(e) {
+    this.style.height = 'auto';
+    this.style.height = (this.scrollHeight) + 'px';
+}
